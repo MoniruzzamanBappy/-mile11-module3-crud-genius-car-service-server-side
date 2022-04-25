c2ec1367cd756bbd83e97202181bee81be9bc919
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-04-25 13:16:22 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -11,6 +12,24 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT (req, res, next){
+  
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, precess.env.TOKEN, (err, decoded)=>{
+    if(err){
+      return res.status(403).send({message: 'authorized forbidden'})
+    }
+    req.decoded = decoded;
+
+    next();
+  })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@bappy-practice-db.nb2hg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -23,6 +42,9 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("GeniusCar").collection("service");
+    const orderCollection = client.db("GeniusCar").collection("order");
+
+
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -52,6 +74,37 @@ async function run() {
         const result = await serviceCollection.deleteOne(query);
         res.send(result);
     });
+
+    // order api
+// post or add data into database
+    app.post('/order', async (req, res)=>{
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // get all data from db
+    app.get('/order', verifyJWT, async (req, res)=>{
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+      if(email ===decodedEmail){
+        const query = {email: email};
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      }
+      else{
+         res.status(403).send({message: 'authorized forbidden'})
+      }
+    });
+
+
+    // auth
+    app.post('/login', async (req, res)=>{
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.TOKEN, {expiresIn: '1d'})
+      res.send({accessToken});
+    })
 
   } 
   
